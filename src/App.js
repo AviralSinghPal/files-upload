614b99214cf2f4d47ae61784161801578510ff7d
Aviral Singh Pal <aviralpal00007@gmail.com>
2023-02-13 01:57:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />   
+          <Route path="/login" element={<Login />} />   
           <Route path="/dashboard" element={<UserDashboard />} />
         </Routes>
       </Router>
